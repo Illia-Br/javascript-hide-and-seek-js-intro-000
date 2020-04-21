9ce7f7509a76d446912bfdd7a9f42aff7c239540
Illia Brytun <illiabrytun@gmail.com>
2020-04-21 02:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var find = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < find.length; i++) {
+  find[i].innerHTML = (parseInt(find[i].innerHTML, 10) + n).toString();
+ }
+}
+
+function deepestChild() {
+
+}
