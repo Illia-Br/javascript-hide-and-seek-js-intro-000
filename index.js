31cf7952f3701ba04c8e84afc58d6b4772e0e5c8
Illia Brytun <illiabrytun@gmail.com>
2020-04-21 02:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var find = document.querySelectorAll('.ranked-list')
   for (let i = 0; i < find.length; i++) {
-    
+
   find[i].innerHTML = (parseInt(find[i].innerHTML, 10) + n).toString();
  }
  return find
